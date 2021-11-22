9dd01e94f3bf9753237be680b6fd8ce05ffaf519
oumaima <oumaimaabouelhaitam@gmail.com>
2021-11-22 13:45:15 +0100

    creat struct

---
+++ projet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+struct Gestion_Bancaire{
+    char CIN[100];
+    char nom[100];
+    char prenom[100];
+    float montant;
+};
+struct Gestion_Bancaire compt1;
+
+
+
+int main()
+{
+    
+
+    return 0;
+}
